61d01415ab0f689d570538ea6a478580eac307ab
B2H990 <anarchya2h@gmail.com>
2021-07-18 00:59:09 -0400

    Fixed crash

--- src/main/java/net/numaclient/numa/Dupe.java
+++ src/main/java/net/numaclient/numa/Dupe.java
@@ -6,11 +6,14 @@ import net.minecraft.text.Text;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 
+import java.util.Objects;
+
 public class Dupe {
     Timer timer = new Timer();
     MinecraftClient mc = MinecraftClient.getInstance();
     boolean shouldPlace = false;
     int delay;
+
     public void onCommand(String message) {
         String[] args = message.replace(".dupe ", "").split(" ");
         if (args[0].contains("swing")) {
@@ -19,11 +22,12 @@ public class Dupe {
                 shouldPlace = true;
                 delay = Integer.parseInt(args[2]);
                 for (int i = 0; i < Integer.parseInt(args[1]); i++) {
+                    if (mc.player == null || mc.world == null) return;
                     mc.player.swingHand(Hand.MAIN_HAND);
                     mc.player.swingHand(Hand.OFF_HAND);
                 }
             } catch (Exception e) {
-                mc.player.sendMessage(Text.of("Invalid Args Provided"), false);
+                if (mc.player != null) mc.player.sendMessage(Text.of("Invalid Args Provided"), false);
             }
         }
         if (args[0].contains("message")) {
@@ -31,16 +35,17 @@ public class Dupe {
                 timer.reset();
                 shouldPlace = true;
                 delay = Integer.parseInt(args[1]);
-                mc.getNetworkHandler().sendPacket(new ChatMessageC2SPacket(" "));
+                Objects.requireNonNull(mc.getNetworkHandler()).sendPacket(new ChatMessageC2SPacket(" "));
             } catch (Exception e) {
-                mc.player.sendMessage(Text.of("Invalid Args Provided"), false);
+                if (mc.player != null) mc.player.sendMessage(Text.of("Invalid Args Provided"), false);
             }
         }
     }
 
 
     public void onTick() {
-        if (!shouldPlace) return;
+        if (mc.world == null || mc.player == null || mc.interactionManager == null) return;
+            if (!shouldPlace) return;
         if (timer.passed(delay)) {
             mc.interactionManager.interactBlock(mc.player, mc.world, Hand.MAIN_HAND, (BlockHitResult) mc.player.raycast(40, mc.getTickDelta(), false));
             timer.reset();
